6c8c67050289848e037892b66ea8c8adf7cbe53b
Nanoxsec <lucas2000bss@gmail.com>
2025-04-09 10:25:05 -0300

    Update brutessh.py

--- brutessh.py
+++ brutessh.py
@@ -80,7 +80,6 @@ if host_ip_dns and host_tports:
 					password_sh = credenciais[1].strip()
 					futures.append(executor.submit(tentar_login, host_ip_dns, host_tports, username_sh, password_sh))
 
-
 				for future in as_completed(futures):
 					if found_event.is_set():
 						break
